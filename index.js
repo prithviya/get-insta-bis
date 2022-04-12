67f2938fcbf70a9ac0636b9dd895eff78d3b7ea9
prithviya <geethaa14367@gmail.com>
2022-04-12 14:29:16 +0530

    33 commit

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ function setFocus(on) {
       });
     }
   }
+  $(window).load(function(){
+		$(".col-3 input").val("");
+		
+		$(".input-effect input").focusout(function(){
+			if($(this).val() != ""){
+				$(this).addClass("has-content");
+			}else{
+				$(this).removeClass("has-content");
+			}
+		})
+	});
